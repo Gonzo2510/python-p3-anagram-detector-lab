89445b453b4654e21784ce4cccb3148db1cdf156
Jacob Gonzales <jagonz@live.com>
2023-11-02 18:22:33 -0500

    complete lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,15 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, list):
+        matching_items = []
+        for item in list:
+            if sorted(self.word) == (sorted(item)):
+                matching_items.append(item)
+        return matching_items
+
+
+
+
+# Anagram('listen').match(['enlists', 'google', 'inlets', 'banana'])
